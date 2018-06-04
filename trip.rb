8435bbbcee4af1f4ac97e633e367bbb73c628361
gabriellapowell <soccerplayer60@icloud.com>
2018-06-04 20:46:26 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -7,9 +7,9 @@ puts "You are going on a trip to #{stay}."
   #don't forget to Capitilize the input
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 puts "What food do you like to eat?"
-eats = gets.strip.capitalize
+eats = gets.strip
 puts "I love #{eats}"
 puts "How many nights will you stay for?"
-nights = gets.strip.capitalize
+nights = gets.strip
 puts "You will be going on a trip to #{stay}, you will be eating #{eats} and you will be staying for #{nights} nights"
 # Lastly, puts it all back by interpolating these values in a string
