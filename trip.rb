6d60d3e03a80de06af74db03fd2614f51c65eae4
gabriellapowell <soccerplayer60@icloud.com>
2018-06-04 20:47:04 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,5 +1,5 @@
 # Code your prompts here!
-Let's plan your trip! Where would you like to stay?
+puts "Let's plan your trip! Where would you like to stay?"
 # First, puts out a string asking where the tourist would like to stay.
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
 stay = gets.chomp.capitalize
